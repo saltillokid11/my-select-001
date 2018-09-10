e523936cf49a4974d14b74afe0c57f6f638ce1a8
Tanner Lancaster <tanner@flatironschool.com>
2018-09-10 01:59:10 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
-def my_select(collection)
-  i = 0;
-  arr = [];
-  while i < collection.length
-    if yield(collection[i])
-      select << arr[i]
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
     end
     i+=1
   end
